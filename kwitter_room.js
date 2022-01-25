bc526815f4e8c459af9fcd2a485a3a4eaf9e6693
SaiGaneshKaushik <86047065+SaiGaneshKaushik@users.noreply.github.com>
2022-01-25 08:56:34 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCKJooLzvaGh13ExW-Ps0P0LovJs8yFGr4",
+    authDomain: "let-chat-app.firebaseapp.com",
+    databaseURL: "https://let-chat-app-default-rtdb.firebaseio.com",
+    projectId: "let-chat-app",
+    storageBucket: "let-chat-app.appspot.com",
+    messagingSenderId: "777122885810",
+    appId: "1:777122885810:web:56afff90cc999b4ffa99ab"
+  };
+  
+  initializeApp(firebaseConfig);
